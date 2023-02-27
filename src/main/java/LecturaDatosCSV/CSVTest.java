41bd2ea4bad195fd5d0fae8cf58de0cd0b76cb2e
Daniel Carpintero <114917207+DanielCarpinteroGarcia@users.noreply.github.com>
2023-02-27 20:19:57 +0100

    Create CSVTest.java

---
+++ src/main/java/LecturaDatosCSV/CSVTest.java
@@ -0,0 +1,14 @@
+package LecturaDatosCSV;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CSVTest {
+
+    @org.junit.jupiter.api.Test
+    void readTable() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void readTableWithLabels() {
+    }
+}
