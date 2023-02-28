24c2aaa8196be5e1ee3346f5d39f8acb860c25dd
daniel <al418152@uji.es>
2023-02-28 20:12:18 +0100

    update

---
+++ src/main/java/AlgoritmoKNN/AlgoritmoKNN.java
@@ -0,0 +1,15 @@
+package AlgoritmoKNN;
+
+import LecturaDatosCSV.TableWithLabels;
+
+import java.util.List;
+
+public class AlgoritmoKNN {
+    public void train(TableWithLabels data){
+        
+    }
+
+    public Integer estimate(List<Double> data){
+
+    }
+}
