0c90208b6745dee8677f0831a46d291c47cd3134
daniel <al418152@uji.es>
2023-02-28 07:19:45 +0100

    update

--- src/main/java/LecturaDatosCSV/CSVTest.java
+++ src/main/java/LecturaDatosCSV/CSVTest.java
@@ -1,6 +1,7 @@
 package LecturaDatosCSV;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,16 +14,28 @@ class CSVTest {
         String fichero = "miles_dollars.csv";
         String fichero2 = "iris.csv";
         Table obtenido = objeto.readTable(fichero);
+
         System.out.print("Número de ejemplares o filas leídos");
         assertEquals(25, obtenido.columnas.size());
+
         System.out.print("Número de columnas leído");
         int nColumnas = 0;
         for(Row r: obtenido.columnas){
             nColumnas += r.data.size();
         }
         assertEquals(50, nColumnas);
+
         System.out.print("Nombre de las etiquetas de las cabeceras");
-        
+        List<String> l = new ArrayList<>();
+        l.add("Miles");
+        l.add("Dollars");
+        assertEquals(l, obtenido.headers);
+
+        System.out.print("Recuperar contenidos de las tablas guardadas.");
+        List<Double> lista = new ArrayList<>();
+        lista.add();
+        lista.add();
+        assertEquals();
     }
 
     @org.junit.jupiter.api.Test
