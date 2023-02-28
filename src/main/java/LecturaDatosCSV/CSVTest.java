3e21949925a058d9617297b4dc53c26eb4dd9fa0
daniel <al418152@uji.es>
2023-02-28 06:50:27 +0100

    update

--- src/main/java/LecturaDatosCSV/CSVTest.java
+++ src/main/java/LecturaDatosCSV/CSVTest.java
@@ -6,6 +6,14 @@ class CSVTest {
 
     @org.junit.jupiter.api.Test
     void readTable() {
+        CSV objeto = new CSV();
+        String fichero = "miles_dollars.csv";
+        String fichero2 = "iris.csv";
+        Table obtenido = objeto.readTable(fichero);
+        Table resultado;
+        TableWithLabels obt = objeto.readTableWithLabels(fichero2);
+        TableWithLabels resul;
+        assertEquals();
     }
 
     @org.junit.jupiter.api.Test
