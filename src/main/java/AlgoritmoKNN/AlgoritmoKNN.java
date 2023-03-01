0d994aa098e056be3db87e6fce9d0ec7f531dafe
daniel <al418152@uji.es>
2023-03-01 07:34:23 +0100

    update

--- src/main/java/AlgoritmoKNN/AlgoritmoKNN.java
+++ src/main/java/AlgoritmoKNN/AlgoritmoKNN.java
@@ -1,15 +1,41 @@
 package AlgoritmoKNN;
 
+import LecturaDatosCSV.Row;
+import LecturaDatosCSV.RowWithLabel;
 import LecturaDatosCSV.TableWithLabels;
 
 import java.util.List;
 
 public class AlgoritmoKNN {
+    private TableWithLabels t;
     public void train(TableWithLabels data){
-        
+        setT(data);
     }
 
     public Integer estimate(List<Double> data){
+        TableWithLabels t = getT();
+        int menor = 1000000;
+        int clase = -1;
+        for(int i = 0; i < t.getColumnas().size(); i++){
+            RowWithLabel r = t.getRowAt(i);
+            List<Double> l = r.getData();
+            int distancia = 0;
+            for(int j = 0; j < l.size(); j++){
+                distancia += Math.sqrt(Math.pow(data.get(i) - l.get(i), 2));
+            }
+            if(distancia < menor){
+                menor = distancia;
+                clase = r.getNumberClass();
+            }
+        }
+        return clase;
+    }
 
+    public TableWithLabels getT(){
+        return t;
+    }
+
+    public void setT (TableWithLabels table){
+        t = table;
     }
 }
