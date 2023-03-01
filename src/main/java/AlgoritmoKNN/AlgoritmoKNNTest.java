5a4254c2a0c60cd0218063c5cde01873d06de5ac
daniel <al418152@uji.es>
2023-03-01 07:43:19 +0100

    update

--- src/main/java/AlgoritmoKNN/AlgoritmoKNNTest.java
+++ src/main/java/AlgoritmoKNN/AlgoritmoKNNTest.java
@@ -2,23 +2,21 @@ package AlgoritmoKNN;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AlgoritmoKNNTest {
 
     @Test
-    void train() {
-    }
-
-    @Test
     void estimate() {
+        List<Double> lista = new ArrayList<>();
+        lista.add(4.6);
+        lista.add(3.1);
+        lista.add(1.4);
+        lista.add(0.3);
+        assertEquals(0, estimate(lista));
     }
 
-    @Test
-    void getT() {
-    }
-
-    @Test
-    void setT() {
-    }
 }
