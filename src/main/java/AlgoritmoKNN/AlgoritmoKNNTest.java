264dfd630accb886faaa5e795ca8fa9b0ec7f496
daniel <al418152@uji.es>
2023-03-01 07:36:34 +0100

    update

---
+++ src/main/java/AlgoritmoKNN/AlgoritmoKNNTest.java
@@ -0,0 +1,24 @@
+package AlgoritmoKNN;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlgoritmoKNNTest {
+
+    @Test
+    void train() {
+    }
+
+    @Test
+    void estimate() {
+    }
+
+    @Test
+    void getT() {
+    }
+
+    @Test
+    void setT() {
+    }
+}
