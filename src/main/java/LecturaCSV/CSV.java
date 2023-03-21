b4c6cf8defa47dbc9c1013327aa66dcce20bc667
daniel <al418152@uji.es>
2023-03-21 11:37:46 +0100

    Update CSV.java

--- src/main/java/LecturaCSV/CSV.java
+++ src/main/java/LecturaCSV/CSV.java
@@ -37,15 +37,13 @@ public class CSV {
     }
 
     public static TableWithLabels readTableWithLabels(String nombreFichero) {
-        List<String> headers = new ArrayList<>();
-        List<Row> rows = new ArrayList<>();
-        Map<String,Integer> labelsToIndex = new HashMap<>();
+        TableWithLabels tableWithLabels = new TableWithLabels();
 
         try {
             Scanner sc = new Scanner (new File(nombreFichero));
             String line = sc.nextLine();
             String[] campos = line.split(",");
-            headers.addAll(Arrays.asList(campos));
+            tableWithLabels.addHeaders(campos);
 
             while(sc.hasNextLine()){
                 List<Double> data = new ArrayList<>();
@@ -54,19 +52,13 @@ public class CSV {
                 for(int i = 0; i<campos.length-1; i++) {
                     data.add(Double.parseDouble(campos[i]));
                 }
-
                 String label = campos[campos.length - 1];
-                int numberClass = labelsToIndex.size();
-                if(!labelsToIndex.containsKey(label)) {
-                    labelsToIndex.put(label,numberClass);
-                }
-                rows.add(new RowWithLabel(data, labelsToIndex.get(label)));
-
+                tableWithLabels.addRow(data,label);
             }
         } catch (FileNotFoundException e) {
             System.out.println("Fichero no encontrado");
         }
-        return new TableWithLabels(headers,rows,labelsToIndex);
+        return tableWithLabels;
     }
 
 }
