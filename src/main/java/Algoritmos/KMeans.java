67ff0854a48921a83dc036cb1756642bb50ca768
daniel <al418152@uji.es>
2023-03-28 11:18:23 +0200

    Update

--- src/main/java/Algoritmos/KMeans.java
+++ src/main/java/Algoritmos/KMeans.java
@@ -14,15 +14,14 @@ public class KMeans {
     private int numClusters;
     private int numIterations;
     private int seed;
-
     private List<Row> representantes;
 
-    public KMeans(int numClusters, int numIterations, long seed){
-        numClusters = numClusters;
-        numIterations = numIterations;
-        seed = seed;
+    public KMeans(int nc, int ni, int s, List<Row> r){
+        numClusters = nc;
+        numIterations = ni;
+        seed = s;
+        representantes = r;
     }
-
 
     public Integer estimate(List<Double> dato){
 
@@ -38,14 +37,18 @@ public class KMeans {
         }
     }
 
-    public int asignar_grupo(List<Row> r, Table d){
+    public int asignar_grupo(Table d){
         for(int i = 0; i < d.getRows().size(); i++){
             Row r1 = d.getRowAt(i);
-            for(int j = 0; j < r.size(); j++){
-                Row r2 = r.get(j);
+            for(int j = 0; j < representantes.size(); j++){
+                Row r2 = representantes.get(j);
                 
             }
         }
+    }
+
+    public double distancia(Row row1, Row row2){
+        
     }
 
 
