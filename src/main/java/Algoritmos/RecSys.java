c3d76b4bc2c4d65d992b272b31a94635dc667ac5
Daniel Carpintero <114917207+DanielCarpinteroGarcia@users.noreply.github.com>
2023-04-02 11:29:06 +0200

    update

--- src/main/java/Algoritmos/RecSys.java
+++ src/main/java/Algoritmos/RecSys.java
@@ -1,5 +1,6 @@
 package Algoritmos;
 
+import Rows.Row;
 import Tables.Table;
 
 import java.util.ArrayList;
@@ -7,17 +8,21 @@ import java.util.List;
 
 public class RecSys {
 
-    private Table traindData;
+    private Table trainData;
     private Table testData;
     private String nameLikedItem;
     private List<String> testItemNames;
     private List<Integer> recomendaciones = new ArrayList<>();
+    private Algorithm algorithm;
 
     public RecSys(Algorithm algorithm){
-        
+        this.algorithm = algorithm;
     }
     public void train(Table trainData){
+        this.trainData = trainData;
+        algorithm.train(trainData);
     }
+
     public void run(Table testData, List<String> testItemNames){
         this.testData = testData;
         this.testItemNames = testItemNames;
@@ -25,15 +30,23 @@ public class RecSys {
     }
 
     private void estimate(Table testData){
-
+        for(int i = 0; i < testData.getRows().size(); i++){
+            recomendaciones.add(i, (Integer) algorithm.estimate(testData));
+        }
     }
 
     private int findName(String nameItem){
-
+        int i = -1;
+        for(i = 0; i < testItemNames.size(); i++){
+            if(nameItem.equals(testItemNames.get(i))){
+                return i;
+            }
+        }
+        return i;
     }
 
     private void selectItems(int idxLikedItem, int labelLikedItem, int numRec){
-
+        
     }
 
     private List<String> getNamesSelectedItems(){
@@ -41,6 +54,10 @@ public class RecSys {
     }
 
     public List<String> recommend(String nameLikedItem, int numRecommendations){
+        int idx = findName(nameLikedItem);
+        Row elemento_idx = testData.getRowAt(idx);
+        int lbl = (Integer) algorithm.estimate(elemento_idx.getData());
+        selectItems(idx, lbl, numRecommendations);
     }
 
 }
