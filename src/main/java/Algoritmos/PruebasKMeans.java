f6a4d585f4042922d582cd21c33e04de1999cc15
Daniel Carpintero <114917207+DanielCarpinteroGarcia@users.noreply.github.com>
2023-04-02 15:20:07 +0200

    update

---
+++ src/main/java/Algoritmos/PruebasKMeans.java
@@ -0,0 +1,49 @@
+package Algoritmos;
+
+import Rows.Row;
+import Tables.Table;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class PruebasKMeans {
+
+    public void saveTable(Table t, String filename) throws IOException {
+        try {
+            FileWriter fw = new FileWriter(filename);
+            for (int i=0; i<t.getRows().size(); i++)
+            {
+                Row row = t.getRowAt(i);
+                List<Double> datos = row.getData();
+                int j=0;
+                for (; j<datos.size()-1; j++)
+                {
+                    fw.write(datos.get(j).toString());
+                    fw.write(",");
+                }
+                fw.write(datos.get(j).toString());
+                fw.write("\n");
+            }
+            fw.close();
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
+
+    void savePredictions()
+    {
+        Table datos_out = new Table();
+        for (int i=0; i<datos.getNumRows(); i++) {
+            List<Double> data = datos.getRowAt(i).getData();
+            data.add((double)kmeans.estimate(data));
+            datos_out.addRow(data);
+        }
+        try {
+            saveTable(datos_out, dataset + "_out.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
