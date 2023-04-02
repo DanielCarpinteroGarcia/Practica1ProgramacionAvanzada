f2c07a972ee0e905c605421348c3eea47f0ea3c3
Daniel Carpintero <114917207+DanielCarpinteroGarcia@users.noreply.github.com>
2023-04-02 10:46:50 +0200

    update

--- src/main/java/Algoritmos/RecSys.java
+++ src/main/java/Algoritmos/RecSys.java
@@ -2,15 +2,26 @@ package Algoritmos;
 
 import Tables.Table;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RecSys {
+
+    private Table traindData;
+    private Table testData;
+    private String nameLikedItem;
+    private
+
     public RecSys(Algorithm algorithm){
         
     }
     public void train(Table trainData){
     }
     public void run(Table testData, List<String> testItemNames){
+        List<Integer> l = new ArrayList<>();
+        for(int i = 0; i < testData.getRows().size(); i++){
+            l.add(i, estimate());
+        }
     }
     public List<String> recommend(String nameLikedItem, int numRecommendations){
     }
