0a973341c89b6f64edc79455bbbcad15adaa823d
daniel <al418152@uji.es>
2023-04-25 11:06:53 +0200

    update

--- src/main/java/LecturaCSV/CSVLabeledFileReader.java
+++ src/main/java/LecturaCSV/CSVLabeledFileReader.java
@@ -1,4 +1,38 @@
 package LecturaCSV;
 
-public class CSVLabeledFileReader {
+public class CSVLabeledFileReader extends CSVUnlabeledFileReader{
+    public CSVLabeledFileReader(String f) {
+        super(f);
+    }
+
+    @Override
+    public void openSource(String source) {
+
+    }
+
+    @Override
+    public void processHeaders(String headers) {
+
+    }
+
+    @Override
+    public void processData(String data) {
+
+    }
+
+    @Override
+    public void closeSource() {
+
+    }
+
+    @Override
+    public boolean hasMoreData() {
+        return false;
+    }
+
+    @Override
+    public String getNextData() {
+        return null;
+    }
+
 }
--- src/main/java/LecturaCSV/CSVUnlabeledFileReader.java
+++ src/main/java/LecturaCSV/CSVUnlabeledFileReader.java
@@ -1,5 +1,38 @@
 package LecturaCSV;
 
 public class CSVUnlabeledFileReader extends ReaderTemplate {
-    
+
+    public CSVUnlabeledFileReader(String f) {
+        super(f);
+    }
+
+    @Override
+    public void openSource(String source) {
+
+    }
+
+    @Override
+    public void processHeaders(String headers) {
+
+    }
+
+    @Override
+    public void processData(String data) {
+
+    }
+
+    @Override
+    public void closeSource() {
+
+    }
+
+    @Override
+    public boolean hasMoreData() {
+        return false;
+    }
+
+    @Override
+    public String getNextData() {
+        return null;
+    }
 }
